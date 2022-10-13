3fa2c5926e08f1e589942b669ecb6599b23f437b
Tadeh Anbarchian <76681722+devman-todd@users.noreply.github.com>
2022-10-12 22:03:02 -0700

    Add files via upload

---
+++ IT244_Anbarchian_Unit2_Assignment.py
@@ -0,0 +1,29 @@
+
+str1 = 'Your color choices are red, blue, green, white or yellow.'
+print(str1)
+
+str2 = 'Enter a color from the list above: '
+print(str2)
+
+userColor = input()
+
+userColor = userColor.lower()
+
+validcolor = True
+if userColor == 'red':
+    spanishcolor = 'rojo'
+elif userColor == 'blue':
+    spanishColor = 'azul'
+elif userColor == 'green':
+    spanishColor = 'verde'
+elif userColor == 'white':
+    spanishcolor = 'blanco'
+elif userColor == 'yellow':
+    spanishcolor = 'amarillo'
+else:
+    validcolor = False
+
+if validcolor:
+    print('The color', userColor, 'in Spanish is ' + spanishcolor)
+else:
+    print('That is not a valid color for this program. Ese no es un color Valido')
